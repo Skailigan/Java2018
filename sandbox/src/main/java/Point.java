f0ae1edf719e7c236f634d945575778a3db5fea8
Ekaterina Zubarev <skailigan@gmail.com>
2018-05-04 19:56:49 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,19 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void main(String[] args) {
+        double d = Point.distance(new Point(1,2 ), new Point(3,4));
+        System.out.println(d);
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
+}
+
