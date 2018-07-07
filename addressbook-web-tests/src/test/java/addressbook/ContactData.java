98d8c689599fc81f474529909075cfa8c350f4b5
Ekaterina Zubarev <skailigan@gmail.com>
2018-07-07 22:54:30 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге

---
+++ addressbook-web-tests/src/test/java/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package addressbook;
+
+public class ContactData {
+  private final String contactfirstName;
+  private final String contactLastName;
+  private final String contactMainAddress;
+  private final String contactHomePhoneNumber;
+  private final String contactEmail;
+  private final String birthDayNumber;
+  private final String birthMonthNumber;
+  private final String birthYear;
+
+  public ContactData(String contactfirstName, String contactLastName, String contactMainAddress, String contactHomePhoneNumber, String contactEmail, String birthDayNumber, String birthMonthNumber, String birthYear) {
+    this.contactfirstName = contactfirstName;
+    this.contactLastName = contactLastName;
+    this.contactMainAddress = contactMainAddress;
+    this.contactHomePhoneNumber = contactHomePhoneNumber;
+    this.contactEmail = contactEmail;
+    this.birthDayNumber = birthDayNumber;
+    this.birthMonthNumber = birthMonthNumber;
+    this.birthYear = birthYear;
+  }
+
+  public String getContactfirstName() {
+    return contactfirstName;
+  }
+
+  public String getContactLastName() {
+    return contactLastName;
+  }
+
+  public String getContactMainAddress() {
+    return contactMainAddress;
+  }
+
+  public String getContactHomePhoneNumber() {
+    return contactHomePhoneNumber;
+  }
+
+  public String getContactEmail() {
+    return contactEmail;
+  }
+
+  public String getBirthDayNumber() {
+    return birthDayNumber;
+  }
+
+  public String getBirthMonthNumber() {
+    return birthMonthNumber;
+  }
+
+  public String getBirthYear() {
+    return birthYear;
+  }
+}
