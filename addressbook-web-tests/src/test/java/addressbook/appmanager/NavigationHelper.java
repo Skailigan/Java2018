6bb1b500964dd08f154f911c3018cea9c58171e2
Ekaterina Zubarev <skailigan@gmail.com>
2018-07-13 14:51:34 +0300

    NavigationHelper optimized

--- addressbook-web-tests/src/test/java/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/NavigationHelper.java
@@ -2,7 +2,6 @@ package addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
 
@@ -13,13 +12,21 @@ public class NavigationHelper extends HelperBase {
 
 
   public void gotoGroupPage() {
+    if(isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new")) )  {
+      return;
+    }
 
     click(By.linkText("groups"));
 
   }
 
   public void gotoHomePage() {
-    click(By.xpath("//*[@id=\"nav\"]/ul/li[1]/a"));
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
+    click(By.linkText("home"));
   }
 
 }
